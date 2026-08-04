f6146bc66e8e75974ae421982f59dc516fa87326
Feiyu Li <lfypku@gmail.com>
2026-08-04 12:10:28 +0800

    Mediova｜阻断裁剪控件递归反馈

---
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_crop_sync_guard_windows.go
@@ -0,0 +1,111 @@
+//go:build windows
+
+package main
+
+import (
+	"sync"
+	"sync/atomic"
+	"syscall"
+)
+
+const (
+	v452CropSyncParentSubclassID = 0x4553
+	v452CropSyncEditSubclassID   = 0x4554
+	v452ENChange                 = 0x0300
+)
+
+type v452CropSyncState struct {
+	depth atomic.Int32
+}
+
+var (
+	v452CropSyncEventCB      uintptr
+	v452CropSyncParentCB     uintptr
+	v452CropSyncEditCB       uintptr
+	v452CropSyncHook         uintptr
+	v452CropSyncStates       sync.Map // map[uintptr]*v452CropSyncState, keyed by dialog HWND
+	v452CropSyncParents      sync.Map // map[uintptr]bool
+	v452CropSyncEdits        sync.Map // map[uintptr]bool
+	v452CropSyncGetParent    = user32.NewProc("GetParent")
+)
+
+func init() {
+	v452CropSyncEventCB = syscall.NewCallback(v452CropSyncEventProc)
+	v452CropSyncParentCB = syscall.NewCallback(v452CropSyncParentSubclassProc)
+	v452CropSyncEditCB = syscall.NewCallback(v452CropSyncEditSubclassProc)
+	v452CropSyncHook, _, _ = v452SetWinEventHook.Call(
+		v452EventObjectCreate,
+		v452EventObjectShow,
+		0,
+		v452CropSyncEventCB,
+		0,
+		0,
+		v452WineventOutofcontext,
+	)
+}
+
+func v452CropSyncEventProc(hook, event, hwnd, idObject, idChild, eventThread, eventTime uintptr) uintptr {
+	v452InstallCropSyncGuard(activeTrim)
+	return 0
+}
+
+func v452InstallCropSyncGuard(d *trimDialog) {
+	if d == nil || d.hwnd == 0 {
+		return
+	}
+	if _, loaded := v452CropSyncParents.LoadOrStore(d.hwnd, true); !loaded {
+		v452CropSyncStates.Store(d.hwnd, &v452CropSyncState{})
+		v452SetWindowSubclass.Call(d.hwnd, v452CropSyncParentCB, v452CropSyncParentSubclassID, 0)
+	}
+	for _, hwnd := range []uintptr{d.hX, d.hY, d.hW, d.hH} {
+		if hwnd == 0 {
+			continue
+		}
+		if _, loaded := v452CropSyncEdits.LoadOrStore(hwnd, true); !loaded {
+			v452SetWindowSubclass.Call(hwnd, v452CropSyncEditCB, v452CropSyncEditSubclassID, 0)
+		}
+	}
+}
+
+func v452CropSyncParentSubclassProc(hwnd uintptr, message uint32, wParam, lParam, subclassID, refData uintptr) uintptr {
+	if message == WM_COMMAND && int(hiWord(wParam)) == v452ENChange && v452IsCropEditID(int(loWord(wParam))) {
+		if value, ok := v452CropSyncStates.Load(hwnd); ok && value.(*v452CropSyncState).depth.Load() > 0 {
+			return 0
+		}
+	}
+	if message == v452WMNCDestroy {
+		v452RemoveSubclass.Call(hwnd, v452CropSyncParentCB, subclassID)
+		v452CropSyncStates.Delete(hwnd)
+		v452CropSyncParents.Delete(hwnd)
+	}
+	result, _, _ := v452DefSubclassProc.Call(hwnd, uintptr(message), wParam, lParam)
+	return result
+}
+
+func v452CropSyncEditSubclassProc(hwnd uintptr, message uint32, wParam, lParam, subclassID, refData uintptr) uintptr {
+	if message == v452WMSetText {
+		parent, _, _ := v452CropSyncGetParent.Call(hwnd)
+		if value, ok := v452CropSyncStates.Load(parent); ok {
+			state := value.(*v452CropSyncState)
+			state.depth.Add(1)
+			result, _, _ := v452DefSubclassProc.Call(hwnd, uintptr(message), wParam, lParam)
+			state.depth.Add(-1)
+			return result
+		}
+	}
+	if message == v452WMNCDestroy {
+		v452RemoveSubclass.Call(hwnd, v452CropSyncEditCB, subclassID)
+		v452CropSyncEdits.Delete(hwnd)
+	}
+	result, _, _ := v452DefSubclassProc.Call(hwnd, uintptr(message), wParam, lParam)
+	return result
+}
+
+func v452IsCropEditID(id int) bool {
+	switch id {
+	case IDC_CROP_X, IDC_CROP_Y, IDC_CROP_W, IDC_CROP_H:
+		return true
+	default:
+		return false
+	}
+}
