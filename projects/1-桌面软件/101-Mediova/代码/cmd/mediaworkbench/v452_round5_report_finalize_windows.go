b4bcf3b0e7700ce42fab2bbec43b7bb509539617
Feiyu Li <lfypku@gmail.com>
2026-08-04 12:50:52 +0800

    Mediova｜记录裁剪初始状态修复证据

--- projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round5_report_finalize_windows.go
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round5_report_finalize_windows.go
@@ -103,9 +103,11 @@ func (a *application) v452FinalizeRound5ToastReport() error {
 	parents := v452CropSyncParentsOK.Load()
 	edits := v452CropSyncEditsOK.Load()
 	intercepted := v452CropSyncIntercepted.Load()
+	repairs := v452CropSyncRepairs.Load()
 	report.Checks["round5_crop_sync_guard_installed"] = parents >= 2 && edits >= 8
 	report.Checks["round5_crop_sync_guard_intercepted"] = intercepted > 0
-	report.Details["round5_crop_sync_guard"] = fmt.Sprintf("tries=%d parents=%d edits=%d intercepted=%d", tries, parents, edits, intercepted)
+	report.Checks["round5_crop_initial_state_repaired"] = repairs >= 2
+	report.Details["round5_crop_sync_guard"] = fmt.Sprintf("tries=%d parents=%d edits=%d intercepted=%d repairs=%d", tries, parents, edits, intercepted, repairs)
 
 	report.Passed = len(report.Checks) > 0
 	for _, ok := range report.Checks {
