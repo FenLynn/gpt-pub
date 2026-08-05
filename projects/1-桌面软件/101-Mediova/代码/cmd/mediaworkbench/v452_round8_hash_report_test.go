450e922c7191baf18a5ae8e94b96579db7890f5b
Feiyu Li <lfypku@gmail.com>
2026-08-05 15:26:08 +0800

    Mediova｜临时输出第八轮收口文件哈希

---
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round8_hash_report_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"testing"
+)
+
+func TestRound8HashReport(t *testing.T) {
+	files := []string{
+		"v452_round7_main_windows.go",
+		"v452_round7_editor_layout_windows.go",
+		"v452_round7_list_overlay_windows.go",
+		"v452_round7_feedback_logic.go",
+		"v452_round7_feedback_logic_test.go",
+		"v452_round7_feedback_columns_windows.go",
+		"v452_round7_feedback_common_windows.go",
+		"v452_round7_feedback_scroll_windows.go",
+		"v452_round7_feedback_visual_windows.go",
+		"v452_round7_feedback_editor_windows.go",
+		"v452_round7_feedback_timeline_windows.go",
+		"v452_round7_native_closeout_windows.go",
+		"v452_round7_source_test.go",
+		"v452_round7_feedback_source_test.go",
+		"v452_round7_manifest_test.go",
+		"v452_round7_feedback_manifest_test.go",
+		"v452_round8_tooltip_windows.go",
+		"v452_round8_editor_install_windows.go",
+		"v452_round8_list_style_guard_windows.go",
+	}
+	for _, name := range files {
+		data, err := os.ReadFile(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		sum := sha256.Sum256(data)
+		t.Logf("ROUND8_HASH %s  %s", hex.EncodeToString(sum[:]), name)
+	}
+}
