203a39f53a0a7a06e757ffd3301b1345f3db5a57
Feiyu Li <lfypku@gmail.com>
2026-08-05 20:54:37 +0800

    Mediova｜将剪裁稳定性入口交由统一安装事务

--- projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round11_editor_preview_windows.go
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round11_editor_preview_windows.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 	"sync/atomic"
-	"time"
 
 	"mediaworkbench/internal/model"
 )
@@ -15,22 +14,6 @@ var (
 	round11EditorPreviewEnabled = round11HasArgument("--round11-editor-preview")
 	round11EditorPreviewStarted atomic.Bool
 )
-
-func init() {
-	if !round11EditorPreviewEnabled {
-		return
-	}
-	go func() {
-		for attempt := 0; attempt < 600; attempt++ {
-			a := app
-			if a != nil && a.hwnd != 0 && a.controlsReady && round11EditorPreviewStarted.CompareAndSwap(false, true) {
-				a.postUI(func() { round11OpenEditorPreview(a) })
-				return
-			}
-			time.Sleep(20 * time.Millisecond)
-		}
-	}()
-}
 
 func round11HasArgument(target string) bool {
 	for _, argument := range os.Args[1:] {
