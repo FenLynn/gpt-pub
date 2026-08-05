15e940f164f52f31075d5927309e265d39b2cabc
Feiyu Li <lfypku@gmail.com>
2026-08-05 14:36:32 +0800

    Mediova｜固化独立列宽与悬浮滚动条纯逻辑

--- projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round7_feedback_logic.go
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round7_feedback_logic.go
@@ -15,6 +15,18 @@ func round7FeedbackCloneWidths(widths []int) []int {
 	return append([]int(nil), widths...)
 }
 
+func round7FeedbackEqualWidths(left, right []int) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	for i := range left {
+		if left[i] != right[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func (p round7FeedbackColumnProfiles) For(kind model.Kind) []int {
 	if kind == model.KindImage {
 		return round7FeedbackCloneWidths(p.Image)
@@ -26,7 +38,7 @@ func (p *round7FeedbackColumnProfiles) Set(kind model.Kind, widths []int) {
 	if p == nil {
 		return
 	}
-	p.Version = 1
+	p.Version = 2
 	if kind == model.KindImage {
 		p.Image = round7FeedbackCloneWidths(widths)
 		return
@@ -44,6 +56,39 @@ func round7FeedbackHoverIntent(width, height, x, y, edge int, needH, needV bool)
 	showV = needV && x >= width-edge
 	showH = needH && y >= height-edge
 	return showH, showV
+}
+
+func round7FeedbackThumbGeometry(trackStart, trackLength, minimum, maximum, page, position int) (start, length int) {
+	if trackLength <= 0 {
+		return trackStart, 0
+	}
+	rangeLength := maximum - minimum + 1
+	if rangeLength <= 0 || page >= rangeLength {
+		return trackStart, trackLength
+	}
+	length = trackLength * page / rangeLength
+	if length < 24 {
+		length = 24
+	}
+	if length > trackLength {
+		length = trackLength
+	}
+	maxPosition := maximum - page + 1
+	if maxPosition < minimum {
+		maxPosition = minimum
+	}
+	if position < minimum {
+		position = minimum
+	}
+	if position > maxPosition {
+		position = maxPosition
+	}
+	movable := trackLength - length
+	if movable <= 0 || maxPosition == minimum {
+		return trackStart, length
+	}
+	start = trackStart + (position-minimum)*movable/(maxPosition-minimum)
+	return start, length
 }
 
 func round7FeedbackTaskEditable(status model.Status) bool {
