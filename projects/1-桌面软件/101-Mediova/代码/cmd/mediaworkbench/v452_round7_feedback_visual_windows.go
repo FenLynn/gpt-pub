179e0b7f03c33724a995ba2b3513306b88a6d149
Feiyu Li <lfypku@gmail.com>
2026-08-05 14:42:43 +0800

    Mediova｜统一状态灯底部按钮与主进度绘制

--- projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round7_feedback_visual_windows.go
+++ projects/1-桌面软件/101-Mediova/代码/cmd/mediaworkbench/v452_round7_feedback_visual_windows.go
@@ -98,8 +98,7 @@ func round7FeedbackDrawAllDefault(a *application, dis *drawItemStruct) bool {
 	oldFont, _, _ := procSelectObject.Call(dis.HDC, uiFontTitle)
 	procSetBkMode.Call(dis.HDC, TRANSPARENT)
 	procSetTextColor.Call(dis.HDC, color)
-	resetIcon := "↺"
-	procDrawTextW.Call(dis.HDC, uintptr(unsafe.Pointer(p(resetIcon))), ^uintptr(0), uintptr(unsafe.Pointer(&iconRC)), DT_CENTER|DT_VCENTER|DT_SINGLELINE)
+	procDrawTextW.Call(dis.HDC, uintptr(unsafe.Pointer(p("↺"))), ^uintptr(0), uintptr(unsafe.Pointer(&iconRC)), DT_CENTER|DT_VCENTER|DT_SINGLELINE)
 	if oldFont != 0 {
 		procSelectObject.Call(dis.HDC, oldFont)
 	}
@@ -184,6 +183,107 @@ func round7FeedbackDrawStatusChip(a *application, dis *drawItemStruct) bool {
 	return true
 }
 
+func round7FeedbackDrawFooterButton(a *application, dis *drawItemStruct) bool {
+	if a == nil || dis == nil || (dis.HwndItem != a.hStart && dis.HwndItem != a.hPause && dis.HwndItem != a.hStop) {
+		return false
+	}
+	disabled := dis.ItemState&ODS_DISABLED != 0
+	pressed := dis.ItemState&ODS_SELECTED != 0
+	hovered := a.hovered(dis.HwndItem)
+	back, border := colorRef(75, 122, 184), colorRef(55, 99, 160)
+	if dis.HwndItem == a.hPause {
+		back, border = colorRef(222, 139, 24), colorRef(190, 110, 12)
+	} else if dis.HwndItem == a.hStop {
+		back, border = colorRef(204, 66, 59), colorRef(175, 46, 41)
+	}
+	if hovered && !disabled {
+		back = round7BlendColor(back, colorRef(255, 255, 255), .08)
+	}
+	if pressed && !disabled {
+		back = round7BlendColor(back, colorRef(0, 0, 0), .12)
+	}
+	if disabled {
+		back, border = colorRef(239, 242, 246), colorRef(207, 214, 223)
+	}
+	fillSolid(dis.HDC, dis.RcItem, colorRef(250, 251, 253))
+	inner := dis.RcItem
+	inner.Left++
+	inner.Top++
+	inner.Right--
+	inner.Bottom--
+	withRoundedClip(dis.HDC, inner, 5, func() { fillSolid(dis.HDC, inner, back) })
+	drawRoundedBorder(dis.HDC, inner, 5, border)
+
+	iconColor, textColor := colorRef(255, 255, 255), colorRef(255, 255, 255)
+	if disabled {
+		iconColor, textColor = colorRef(132, 143, 156), colorRef(125, 136, 149)
+	}
+	cx := dis.RcItem.Left + scaleDPI(20)
+	cy := (dis.RcItem.Top + dis.RcItem.Bottom) / 2
+	switch dis.HwndItem {
+	case a.hStart:
+		points := []point{
+			{X: cx - scaleDPI(4), Y: cy - scaleDPI(6)},
+			{X: cx - scaleDPI(4), Y: cy + scaleDPI(6)},
+			{X: cx + scaleDPI(6), Y: cy},
+		}
+		round7FillPolygon(dis.HDC, points, iconColor)
+	case a.hPause:
+		if getText(a.hPause) == "继续转换" {
+			points := []point{{X: cx - scaleDPI(4), Y: cy - scaleDPI(6)}, {X: cx - scaleDPI(4), Y: cy + scaleDPI(6)}, {X: cx + scaleDPI(6), Y: cy}}
+			round7FillPolygon(dis.HDC, points, iconColor)
+		} else {
+			fillSolid(dis.HDC, rect{Left: cx - scaleDPI(5), Top: cy - scaleDPI(6), Right: cx - scaleDPI(1), Bottom: cy + scaleDPI(6)}, iconColor)
+			fillSolid(dis.HDC, rect{Left: cx + scaleDPI(2), Top: cy - scaleDPI(6), Right: cx + scaleDPI(6), Bottom: cy + scaleDPI(6)}, iconColor)
+		}
+	case a.hStop:
+		fillSolid(dis.HDC, rect{Left: cx - scaleDPI(5), Top: cy - scaleDPI(5), Right: cx + scaleDPI(5), Bottom: cy + scaleDPI(5)}, iconColor)
+	}
+	textRC := dis.RcItem
+	textRC.Left += scaleDPI(34)
+	textRC.Right -= scaleDPI(7)
+	oldFont, _, _ := procSelectObject.Call(dis.HDC, uiFont)
+	procSetBkMode.Call(dis.HDC, TRANSPARENT)
+	procSetTextColor.Call(dis.HDC, textColor)
+	procDrawTextW.Call(dis.HDC, uintptr(unsafe.Pointer(p(getText(dis.HwndItem)))), ^uintptr(0), uintptr(unsafe.Pointer(&textRC)), DT_CENTER|DT_VCENTER|DT_SINGLELINE)
+	if oldFont != 0 {
+		procSelectObject.Call(dis.HDC, oldFont)
+	}
+	return true
+}
+
+func round7FeedbackDrawOverallProgress(a *application, dis *drawItemStruct) bool {
+	if a == nil || dis == nil || dis.HwndItem != a.hProgress {
+		return false
+	}
+	rc := dis.RcItem
+	bar := rect{Left: rc.Left + 1, Top: rc.Top + 2, Right: rc.Right - 1, Bottom: rc.Bottom - 2}
+	fraction := clamp01(a.overallProgress / 100)
+	fill := rect{Left: bar.Left, Top: bar.Top, Right: bar.Left, Bottom: bar.Bottom}
+	withRoundedClip(dis.HDC, bar, 4, func() {
+		fillSolid(dis.HDC, bar, colorRef(248, 250, 252))
+		if fraction > 0 {
+			fill = bar
+			fill.Right = fill.Left + int32(float64(fill.Right-fill.Left)*fraction)
+			if fill.Right > bar.Right {
+				fill.Right = bar.Right
+			}
+			if a.overallPaused {
+				drawHorizontalGradient(dis.HDC, fill, colorRef(235, 237, 240), colorRef(194, 199, 207))
+			} else {
+				drawHorizontalGradient(dis.HDC, fill, colorRef(151, 196, 245), colorRef(58, 122, 214))
+			}
+		}
+	})
+	drawRoundedBorder(dis.HDC, bar, 4, colorRef(218, 223, 230))
+	if a.overallPaused {
+		v452DrawPausedProgressText(dis.HDC, a.overallText, bar)
+	} else {
+		drawContrastCenteredText(dis.HDC, a.overallText, bar, fill, uiFontSmall)
+	}
+	return true
+}
+
 func round7FeedbackDrawFlatLamp(hdc uintptr, x, y, diameter int, lamp, back uintptr) {
 	if hdc == 0 || diameter <= 0 {
 		return
@@ -200,21 +300,10 @@ func round7FeedbackDrawFlatLamp(hdc uintptr, x, y, diameter int, lamp, back uint
 		return
 	}
 	info := round7BitmapInfo{Header: round7BitmapInfoHeader{
-		Size:        uint32(unsafe.Sizeof(round7BitmapInfoHeader{})),
-		Width:       int32(diameter),
-		Height:      -int32(diameter),
-		Planes:      1,
-		BitCount:    32,
-		Compression: round7BI_RGB,
-		SizeImage:   uint32(len(pixels)),
+		Size: uint32(unsafe.Sizeof(round7BitmapInfoHeader{})), Width: int32(diameter), Height: -int32(diameter),
+		Planes: 1, BitCount: 32, Compression: round7BI_RGB, SizeImage: uint32(len(pixels)),
 	}}
-	round7StretchDIBits.Call(
-		hdc,
-		uintptr(x), uintptr(y), uintptr(diameter), uintptr(diameter),
-		0, 0, uintptr(diameter), uintptr(diameter),
-		uintptr(unsafe.Pointer(&pixels[0])), uintptr(unsafe.Pointer(&info)),
-		round7DIBRGBColors, round7SRCCOPY,
-	)
+	round7StretchDIBits.Call(hdc, uintptr(x), uintptr(y), uintptr(diameter), uintptr(diameter), 0, 0, uintptr(diameter), uintptr(diameter), uintptr(unsafe.Pointer(&pixels[0])), uintptr(unsafe.Pointer(&info)), round7DIBRGBColors, round7SRCCOPY)
 }
 
 func round7FeedbackBuildFlatLampPixels(diameter int, lamp, back uintptr) []byte {
