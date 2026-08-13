0970e8d3680843be6fc336ea8cdc8c9622b2a546
Feiyu Li <lfypku@gmail.com>
2026-08-13 11:14:35 +0800

    P103 activate v0.2.7 dashboard

--- projects/1-桌面软件/103-DavBridge/代码/DavBridge/Program.cs
+++ projects/1-桌面软件/103-DavBridge/代码/DavBridge/Program.cs
@@ -19,7 +19,7 @@ internal static class Program
         using var host = new AppHost();
         using var form = new MainForm(host, args.Contains("--background", StringComparer.OrdinalIgnoreCase));
         AppBranding.Apply(form);
-        using var dashboard = UiDashboardV026.Attach(form, host);
+        using var dashboard = UiDashboardV027.Attach(form, host);
         Application.Run(form);
     }
 
