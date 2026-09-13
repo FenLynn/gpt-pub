cce1e4b37f138c9c60d8a6f48a3fbfa0f151589f
Feiyu Li <lfypku@gmail.com>
2026-09-13 18:30:20 +0800

    P103 v0.4.18 self-test operational health summary

--- projects/1-桌面软件/103-DavBridge/代码/DavBridge/Program.cs
+++ projects/1-桌面软件/103-DavBridge/代码/DavBridge/Program.cs
@@ -91,6 +91,7 @@ internal static class Program
             var stateBackupRecovery = ValidateStateBackupRecovery(recoveryRoot);
             var reconcileBackupRecovery = ValidateReconcileBackupRecovery(recoveryRoot);
             var productSidecarBackupRecovery = ProductExperienceV044.ValidateBackupRecoveryForSelfTest(recoveryRoot);
+            var operationalHealthSummary = ProductExperienceV044.ValidateOperationalHealthForSelfTest();
             var runtimeSessionSelfTest = RuntimeSessionV046.ValidateForSelfTest(paths.LocalRoot, paths.TempRoot);
 
             using var host = new AppHost();
@@ -116,6 +117,7 @@ internal static class Program
                 stateBackupRecovery,
                 reconcileBackupRecovery,
                 productSidecarBackupRecovery,
+                operationalHealthSummary,
                 runtimeSessionSelfTest,
                 windowFourThreeMigration,
                 windowPlacementSelfTest,
