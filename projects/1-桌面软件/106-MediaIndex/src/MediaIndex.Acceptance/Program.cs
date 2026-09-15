9b0479004e746031f6f4e55c7294fb2400b9c218
Feiyu Li <lfypku@gmail.com>
2026-09-15 12:56:26 +0800

    P106: add Acceptance app Program.cs

---
+++ projects/1-桌面软件/106-MediaIndex/src/MediaIndex.Acceptance/Program.cs
@@ -0,0 +1,12 @@
+namespace MediaIndex.Acceptance;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.Run(new MainForm());
+    }
+}
