da2ee31f57d73f10ee4ea16be4ce73e88661553a
Feiyu Li <lfypku@gmail.com>
2026-09-15 13:05:02 +0800

    P106: add single entry Acceptance launcher

---
+++ projects/1-桌面软件/106-MediaIndex/start_acceptance.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+set "APP=%~dp0dist\MediaIndex-Acceptance-v0.0.1-win-x64\MediaIndex Acceptance.exe"
+if exist "%APP%" (
+  start "" "%APP%"
+  exit /b 0
+)
+call "%~dp0build_acceptance.bat"
