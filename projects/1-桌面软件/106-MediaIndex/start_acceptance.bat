c4507d381d201c5204f07d737d6bca1c3050d339
Feiyu Li <lfypku@gmail.com>
2026-09-16 12:37:34 +0800

    P106: align local Acceptance launcher with v0.0.4

--- projects/1-桌面软件/106-MediaIndex/start_acceptance.bat
+++ projects/1-桌面软件/106-MediaIndex/start_acceptance.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-set "APP=%~dp0dist\MediaIndex-Acceptance-v0.0.3-win-x64\MediaIndex Acceptance.exe"
+set "APP=%~dp0dist\MediaIndex-Acceptance-v0.0.4-win-x64\MediaIndex Acceptance.exe"
 if exist "%APP%" (
   start "" "%APP%"
   exit /b 0
